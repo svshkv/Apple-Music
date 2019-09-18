3cb2246e15a1c7b948e461665e239bfbf5a663e7
Саша Руцман <rutsman@bk.ru>
2019-09-18 20:28:18 +0300

    + анимация trackImageView

--- Apple Music/Search/TrackDetailView/TrackDetailView.swift
+++ Apple Music/Search/TrackDetailView/TrackDetailView.swift
@@ -27,15 +27,21 @@ class TrackDetailView: UIView {
         avplayer.automaticallyWaitsToMinimizeStalling = false //снижает задержку при загрузке до минимума
         return avplayer
     }()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        trackImageView.layer.cornerRadius = 5
+        trackImageView.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
     }
+    
+    // MARK: - Setup
     
     func set(viewModel: SearchViewModel.Cell) {
         trackTitleLabel.text = viewModel.trackName
         authorTitleLabel.text = viewModel.artistName
         playTrack(previewUrl: viewModel.previewUrl)
+        monitorStartTime()
         let string600 = viewModel.iconUrlString?.replacingOccurrences(of: "100x100", with: "600x600")
         guard let url = URL(string: string600 ?? "") else { return }
         trackImageView.sd_setImage(with: url, completed: nil)
@@ -48,6 +54,31 @@ class TrackDetailView: UIView {
         player.play()
     }
     
+    // MARK: - Time setup
+    
+    private func monitorStartTime() {
+        let time = CMTimeMake(value: 1, timescale: 3)
+        let times = [NSValue(time: time)]
+        player.addBoundaryTimeObserver(forTimes: times, queue: .main) { [weak self] in
+            self?.enlargeTrackImageView()
+        }
+    }
+    
+    // MARK: - Animations
+    
+    private func enlargeTrackImageView() {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.trackImageView.transform = .identity
+        }, completion: nil) // сколько длится анимация, задержка, как резко производится анимация
+    }
+    
+    private func reduceTrackImageView() {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.trackImageView.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+        }, completion: nil)
+    }
+    
+    // MARK: - IBActions
     @IBAction func dragDownButtonTapped(_ sender: Any) {
         self.removeFromSuperview()
     }
@@ -68,9 +99,11 @@ class TrackDetailView: UIView {
         if player.timeControlStatus == .paused {
             player.play()
             playPauseButton.setImage(#imageLiteral(resourceName: "Pause"), for: .normal)
+            enlargeTrackImageView()
         } else {
             player.pause()
             playPauseButton.setImage(#imageLiteral(resourceName: "play").withRenderingMode(.alwaysOriginal), for: UIControl.State.normal)
+            reduceTrackImageView()
         }
     }
 }
